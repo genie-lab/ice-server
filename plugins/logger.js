1e8e33d8f3be45a135520db2f8eb4b0ee6190049
genie-lab <shingenie.lab@gmail.com>
2024-08-24 22:33:03 +0900

    comm db

--- plugins/logger.js
+++ plugins/logger.js
@@ -5,8 +5,8 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "info.log", level: "info" }),
+    // new winston.transports.File({ filename: "error.log", level: "error" }),
+    // new winston.transports.File({ filename: "info.log", level: "info" }),
   ],
 });
 
