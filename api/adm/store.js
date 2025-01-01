a2f794627f02c0e2a8224a3e6079a94a06ec9756
shin genie <shingenie.lab@gmail.com>
2025-01-01 22:18:14 +0900

    admStore

--- api/adm/store.js
+++ api/adm/store.js
@@ -1,45 +1,45 @@
 const router = require("express").Router();
-const mainController = require("./controller/mainController");
-const { modelCall } = require("../util/lib");
+const admStoreController = require("../controller/admStoreController");
+const { modelCall } = require("../../util/lib");
 
 //전체카테고리들
 router.post("/categories", async (req, res) => {
-  const result = await modelCall(mainController.categories, req);
+  const result = await modelCall(admStoreController.categories, req);
   res.json(result);
 });
 //전체목록수
 router.get("/listCount", async (req, res) => {
-  const result = await modelCall(mainController.listCount, req);
+  const result = await modelCall(admStoreController.listCount, req);
   res.json(result);
 });
 //페이지 목록
 router.get("/list", async (req, res) => {
-  const result = await modelCall(mainController.list, req);
+  const result = await modelCall(admStoreController.list, req);
   res.json(result);
 });
 //where절 목록
 router.post("/listByWhere", async (req, res) => {
-  const result = await modelCall(mainController.listByWhere, req);
+  const result = await modelCall(admStoreController.listByWhere, req);
   res.json(result);
 });
 //중복검사
 router.post("/duplCheck", async (req, res) => {
-  const result = await modelCall(mainController.duplCheck, req);
+  const result = await modelCall(admStoreController.duplCheck, req);
   res.json(result);
 });
 //추가
 router.post("/add", async (req, res) => {
-  const result = await modelCall(mainController.add, req);
+  const result = await modelCall(admStoreController.add, req);
   res.json(result);
 });
 //수정
 router.put("/edit", async (req, res) => {
-  const result = await modelCall(mainController.edit, req);
+  const result = await modelCall(admStoreController.edit, req);
   res.json(result);
 });
 //삭제
 router.put("/del", async (req, res) => {
-  const result = await modelCall(mainController.del, req);
+  const result = await modelCall(admStoreController.del, req);
   res.json(result);
 });
 module.exports = router;
