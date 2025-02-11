415f444d69619e9d0ed2a36c743de0610013f7c2
shin genie <shingenie.lab@gmail.com>
2025-02-11 12:26:18 +0900

    pm2 eco

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name:'ice-server',
+    script: 'server.js',
+    instances: 2, // 실행할 프로세스의 인스턴스 수
+    // autorestart: true, // 애플리케이션의 자동 재시작 여부
+    // watch: true, // 파일 변경 감지 및 재시작 여부
+    exec_mode : 'cluster',  // 클러스터 모드
+    wait_ready : true,
+    listen_timeout : 50000,
+    kill_timeout: 50000,
+  }],
+
+  // deploy : {
+  //   production : {
+  //     user : 'SSH_USERNAME',
+  //     host : 'SSH_HOSTMACHINE',
+  //     ref  : 'origin/master',
+  //     repo : 'GIT_REPOSITORY',
+  //     path : 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
+};
