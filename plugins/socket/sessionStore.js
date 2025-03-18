17c75e0731f62c1a2186e867946fbbc8c76813bb
shin genie <shingenie.lab@gmail.com>
2025-03-19 04:35:09 +0900

    member

--- plugins/socket/sessionStore.js
+++ plugins/socket/sessionStore.js
@@ -8,14 +8,14 @@ class InMemorySessionStore extends SessionStore {
   constructor() {
     super();
     this.sessions = new Map();
-    console.log('this.sessions',this.sessions);
+    // console.log('this.sessions',this.sessions);
   }
   findSession(obj) {
-    console.log('sessionStore',obj.sessionID);
+    // console.log('sessionStore',obj.sessionID);
     return this.sessions.get(obj.sessionID);
   }
   saveSession(id, session) {
-    console.log('saveSession',id, session);
+    // console.log('saveSession',id, session);
 
     sessions.set(id, session);
   }
