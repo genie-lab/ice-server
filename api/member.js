dda54b8593795d2b9f66d8ace610285cb8b5680d
shin genie <shingenie.lab@gmail.com>
2025-03-21 21:36:30 +0900

    rere

--- api/member.js
+++ api/member.js
@@ -74,13 +74,13 @@ router.post("/loginLocal", async (req, res) => {
             // 쿠키생성 클라 넣어줌
             res.cookie("token", token, { 
               httpOnly: true,   // JavaScript에서 쿠키 접근 불가능 (보안 강화)
-              secure: true,     // HTTPS에서만 전송됨 (배포 환경에서는 true로 설정)
-              sameSite: 'Lax',  // 크로스사이트 요청 문제 방지 (필요 시 'None' 사용)
+              // secure: true,     // HTTPS에서만 전송됨 (배포 환경에서는 true로 설정)
+              // sameSite: 'Lax',  // 크로스사이트 요청 문제 방지 (필요 시 'None' 사용)
               maxAge: 7 * 24 * 60 * 60 * 1000  // 7일간 유지 (밀리초 단위)
              }); //클라에서 서버로 못옴
+             console.log('>>>>>>>>>>>>>>>>>>>>> memeber : ', member, token);
             const result = { member, token };
             res.json(result);
-            console.log('>>>>>>>>>>>>>>>>>>>>> memeber : ', result);
           } catch (error) {
             res.json({ error });
           }
