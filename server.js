eaed475b79c408aae46aa8f113f9a023e7b6017a
shin genie <shingenie.lab@gmail.com>
2025-03-21 22:23:19 +0900

    reeree

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ require("./plugins/pm2Bus");
   //cors
   const cors = require("cors");
   const corsOptions = {
-    origin:["https://orangewebapp.net","http://localhost:4000","http://localhost:5436"],
-    credentials: true,  
+    origin:["https://orangewebapp.net","http://localhost:4466","http://localhost:5436"],
+    credentials: true,    
   };
   app.use(cors(corsOptions));
 
