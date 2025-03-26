d5b081409794aa64972d68730d778546553ea321
shin genie <shingenie.lab@gmail.com>
2025-03-26 19:19:52 +0900

    re

--- plugins/passport.js
+++ plugins/passport.js
@@ -74,7 +74,7 @@ const passport = function (app) {
   );
 
   app.use(async (req, res, next) => {
-    console.log('토큰ㅣ 있을때 !!!',req.cookies);
+    console.log('토큰ㅣ 있을때 !!!',req);
     const token = req.cookies.token || req.headers.token; // 게시판에서 비회원이 headers에 토큰보냄
     if (!token) return next();
     const { mb_id } = jwt.verify(token);
